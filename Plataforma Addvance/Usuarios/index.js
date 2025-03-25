4f07b63752df3c52e97fbf70348d7e694e9a3b3d
GiGiS05 <lgisellesalomons@gmail.com>
2025-03-25 16:47:11 -0600

    Extraje los archivos de usuario para trabajarlos externamente y no estar lidiando con los push hasta que funcionen

--- Plataforma Addvance/Usuarios/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Aqui estaran todas las funciones y variables que se encuentran relacionadas con el usuario
- * Estas seran heredadas por Usuario, UsuarioEmpresa, UsuarioEmpleado y UsuarioSocio
- */
-import {Usuario} from './Usuario';
-import {UsuarioEmpresa} from './UsuarioEmpresa';
